cbdaef66be720c22e85dcc60f84a050a2fea12ee
Clementina <clementina@example.com>
2026-03-20 11:45:14 -0700

    Fix CI: Remove rogue legacy directories shadowing src/etl_pipeline

--- transformer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""transformer/__init__.py"""
-from .base_transformer import BaseTransformer
-from .cleaner import DataCleaner
-from .mapper import EntityMapper
-
-__all__ = ["BaseTransformer", "DataCleaner", "EntityMapper"]
--- validator/__init__.py
+++
@@ -1,6 +0,0 @@
-"""validator/__init__.py"""
-from .base_validator import BaseValidator
-from .schema_validator import SchemaValidator
-from .id_validator import IDValidator
-
-__all__ = ["BaseValidator", "SchemaValidator", "IDValidator"]
